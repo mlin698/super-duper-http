ea89c09f2e2d97acbcadd585a52a05e9b3c9279e
Megan Lin <megan@Megans-MacBook.local>
2018-12-01 20:22:06 -0600

    added comments

--- Client.py
+++ Client.py
@@ -3,6 +3,7 @@
 import http.client
 import sys
 
+#connects client to the server
 http_server = sys.argv[1]
 conn = http.client.HTTPConnection(http_server)
 
@@ -11,10 +12,12 @@ while 1:
     cmd = input()
     cmd = cmd.split()
 
+    #send command to server
     conn.request(cmd[0], cmd[1])
 
     rsp = conn.getresponse()
 
+    #get server response
     print(rsp.status, rsp.reason)
     data_received = rsp.read()
     data_received = str(data_received).strip('b\"')
